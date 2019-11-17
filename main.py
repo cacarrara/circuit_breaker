5d7c11a939a637c521ce4091ec52a0923037cc6c
Caio Carrara <eu@caiocarrara.com.br>
2019-11-17 19:24:34 -0300

    Adds exception argument

--- main.py
+++ main.py
@@ -7,37 +7,39 @@ class CircuitOpenException(Exception):
     pass
 
 
-def circuit_breaker(_func=None, *, failure_threshold=3, max_open_call=5):
+def circuit_breaker(
+    _func=None, *, exception=Exception, failure_threshold=3, max_open_call=5
+):
     def _circuit_breaker(func):
-        def wrapper(*args, **kwargs):
+        def circuit(*args, **kwargs):
             if (
-                wrapper.failure_count > 0
-                and wrapper.failure_count >= wrapper.failure_threshold
+                circuit.failure_count > 0
+                and circuit.failure_count >= circuit.failure_threshold
             ):
-                wrapper.circuit_open_call_count += 1
+                circuit.circuit_open_call_count += 1
 
-                if wrapper.circuit_open_call_count <= wrapper.max_open_call:
+                if circuit.circuit_open_call_count <= circuit.max_open_call:
                     raise CircuitOpenException(
                         "Circuit is open. No external calls being made."
                     )
                 else:
-                    wrapper.circuit_open_call_count = 0
+                    circuit.circuit_open_call_count = 0
 
             try:
                 result = func(*args, **kwargs)
-                wrapper.failure_count = 0
-                wrapper.circuit_open_call_count = 0
+                circuit.failure_count = 0
+                circuit.circuit_open_call_count = 0
                 return result
-            except requests.ConnectionError as e:
-                wrapper.failure_count += 1
+            except exception as e:
+                circuit.failure_count += 1
                 raise e
 
-        wrapper.failure_threshold = failure_threshold
-        wrapper.failure_count = 0
-        wrapper.circuit_open_call_count = 0
-        wrapper.max_open_call = max_open_call
+        circuit.failure_threshold = failure_threshold
+        circuit.max_open_call = max_open_call
+        circuit.failure_count = 0
+        circuit.circuit_open_call_count = 0
 
-        return wrapper
+        return circuit
 
     if _func is None:
         return _circuit_breaker
@@ -45,7 +47,9 @@ def circuit_breaker(_func=None, *, failure_threshold=3, max_open_call=5):
         return _circuit_breaker(_func)
 
 
-@circuit_breaker
+@circuit_breaker(
+    exception=requests.ConnectionError, failure_threshold=4, max_open_call=3
+)
 def make_call(endpoint):
     return requests.get(endpoint)
 
